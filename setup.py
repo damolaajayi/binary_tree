2bd7f2a8970cca2c08dc44556084310cef695a67
damolaajayi <damolaajayi@gmail.com>
2021-04-04 13:23:17 +0100

    first

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='binary_tree',
+      version='0.1',
+      description='Python module to find value in a binary tree',
+      url='http://github.com/damolaajayi/binary_tree',
+      author='Adedamola Ajayi',
+      author_email='damolaajayi@gmail.com',
+      license='MIT',
+      packages=['binary_tree'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
